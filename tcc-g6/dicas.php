3d48001026986d06f8f683fe1746a3f43c190f0a
ThainaAC <101333470+ThainaAC@users.noreply.github.com>
2023-08-18 20:02:23 -0300

    Add files via upload

---
+++ tcc-g6/dicas.php
@@ -0,0 +1,64 @@
+<?php 
+require_once('./vendor/autoload.php'); 
+$_title = "Beloto & Beloto Redações | Dicas" ;
+getTopPage();
+?>
+
+<main>
+    <h1 style="text-align: center;">CONHEÇA AS METODOLOGIA</h1>
+
+          <!--  Quadros -->
+    <div class="container">
+        <!-- Primeira linha -->
+        <div class="row">
+
+            <div class="col" style="margin: 15px; border-radius: 10px; padding: 20px;">
+            <p style=" text-aling: center; "><img src="img/icon1.png" alt=""   width="100px" height="100px">
+                <h3 class="divsTxt">
+                    Temas de Redações 
+                </h3>
+                </p>
+            </div>
+
+            <div class="col-md-auto" style="margin: 15px; border-radius: 10px; padding: 20px;">
+            <p style=" text-aling: center; "><img src="img/icon2.png" alt=""  width="100px" height="100px"> 
+               <h3 class="divsTxt " >
+                    Gramática
+                </h3>
+             </p>
+            </div>
+
+            <div class="col" style="margin: 15px; border-radius: 10px; padding: 20px;">
+             <p style=" text-aling: center; "><img src="img/icon3.png" alt=""  width="100px" height="100px"> 
+                <h3 class="divsTxt">
+                   Vestibulares
+                </h3>
+            </p>
+            </div>
+            
+        </div>
+
+        <!-- Segunda linha -->
+        <div class="row">
+
+            <div class="col" style="margin: 15px; border-radius: 10px; padding: 20px;">
+             <p style=" text-aling: center; "><img src="img/icon4.png" alt=""  width="100px" height="100px">
+                <h3 class="divsTxt">
+                Competências Avaliativas
+                </h3>
+            </p>
+            </div>
+
+            <div class="col" style=" margin: 15px; border-radius: 10px; padding: 20px;">
+            <p style=" text-aling: center; "> <img src="img/icon5.png" alt=""   width="100px" height="100px">
+            <h3 class="divsTxt">
+                Estrutura Da Redação
+                </h3>
+                </p>
+            </div>
+
+        </div>
+    </div>
+     
+</main>
+<?php getBottomPage() ?>
